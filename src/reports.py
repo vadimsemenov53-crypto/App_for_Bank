ceaa9372d2a2681fbe3029120de018b162953fe6
Vadim Semenov <vadimsemenov53@gmail.com>
2026-02-19 17:23:31 +0300

    clean code src and tests black isort mypy flake8

--- src/reports.py
+++ src/reports.py
@@ -1,24 +1,27 @@
 import json
 import os
-import pandas as pd
 from datetime import datetime
-from typing import Optional
-from pandas import DateOffset
 from functools import wraps
-from typing import Any, Callable
+from typing import Any, Callable, Optional
+
+import pandas as pd
+from pandas import DateOffset
+
 from src.logger_config import get_file_logger
 
-logger = get_file_logger(__name__, 'reports.log')
+logger = get_file_logger(__name__, "reports.log")
 
 PATH = os.path.dirname(os.path.dirname(__file__))
 
+
 def save_reports(filename: str | None = None) -> Callable[..., Any]:
     """
-        Декоратор для функций-отчетов, записывает в файл результат,
-        который возвращает функция, формирующая отчет.
-        Если filename не указан — отчет записывается в файл
-         с дефолтным названием (records.json).
-        """
+    Декоратор для функций-отчетов, записывает в файл результат,
+    который возвращает функция, формирующая отчет.
+    Если filename не указан — отчет записывается в файл
+     с дефолтным названием (records.json).
+    """
+
     def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
         @wraps(func)
         def wrapper(*args: Any, **kwargs: Any) -> Any:
@@ -34,35 +37,34 @@ def save_reports(filename: str | None = None) -> Callable[..., Any]:
             except Exception as error:
                 logger.exception("Ошибка в отчете %s", func.__name__)
                 data_to_save = {
-                    'Функция' : f'{func.__name__}',
-                    "Аргументы" : f'{args}',
-                    "Ключевые аргументы" : f'{kwargs}',
-                    "Тип ошибки" : f"{type(error).__name__}"
+                    "Функция": f"{func.__name__}",
+                    "Аргументы": f"{args}",
+                    "Ключевые аргументы": f"{kwargs}",
+                    "Тип ошибки": f"{type(error).__name__}",
                 }
 
-            file_name = filename if filename else 'reports'
+            file_name = filename if filename else "reports"
 
-            path_to_file = os.path.join(PATH, f'data/{file_name}.json')
+            path_to_file = os.path.join(PATH, f"data/{file_name}.json")
 
             os.makedirs(os.path.dirname(path_to_file), exist_ok=True)
 
             logger.info("Отчет успешно сохранен в %s", path_to_file)
-            with open(path_to_file, 'w', encoding='utf-8') as file:
+            with open(path_to_file, "w", encoding="utf-8") as file:
                 json.dump(data_to_save, file, ensure_ascii=False, indent=2)
 
-
             return data_to_save
+
         return wrapper
+
     return decorator
 
 
-def spending_by_category(transactions: pd.DataFrame,
-                         category: str,
-                         date: Optional[str] = None) -> pd.DataFrame:
+def spending_by_category(transactions: pd.DataFrame, category: str, date: Optional[str] = None) -> pd.DataFrame:
     """Функция возвращает траты по заданной категории
-     за последние три месяца (от переданной даты)
-     Формат даты: (01.01.2021)"""
-    logger.info('Запуск spending_by_category')
+    за последние три месяца (от переданной даты)
+    Формат даты: (01.01.2021)"""
+    logger.info("Запуск spending_by_category")
 
     try:
         if date is None:
@@ -73,27 +75,21 @@ def spending_by_category(transactions: pd.DataFrame,
             end_date = datetime.strptime(date, "%d.%m.%Y")
 
         start_date = end_date - DateOffset(months=3)
-        logger.info('Определяем начало: %s, и конец: %s',
-                    start_date.strftime("%d.%m.%Y"), end_date.strftime("%d.%m.%Y"))
-
+        logger.info(
+            "Определяем начало: %s, и конец: %s", start_date.strftime("%d.%m.%Y"), end_date.strftime("%d.%m.%Y")
+        )
 
         df = transactions.copy()
 
         df["Дата операции"] = pd.to_datetime(df["Дата операции"], dayfirst=True).dt.normalize()
 
-        filtered_df = df[
-            (df["Дата операции"] >= start_date) &
-            (df["Дата операции"] <= end_date)
-        ]
+        filtered_df = df[(df["Дата операции"] >= start_date) & (df["Дата операции"] <= end_date)]
 
-        result = filtered_df[filtered_df['Категория'] == category]
+        result = filtered_df[filtered_df["Категория"] == category]
 
-        logger.info('Возвращаем готовый DateFrame. Завершение работы.')
+        logger.info("Возвращаем готовый DateFrame. Завершение работы.")
         return pd.DataFrame(result)
 
     except Exception as error:
-        logger.error('Произошла ошибка %s', error)
-        return pd.DataFrame({
-            'Произошла ошибка' : f'{error}'
-        })
-
+        logger.error("Произошла ошибка %s", error)
+        return pd.DataFrame({"Произошла ошибка": f"{error}"})
--- tests/test_reports.py
+++ tests/test_reports.py
@@ -1,53 +1,46 @@
 import json
+from datetime import datetime
+
 import pandas as pd
 import pytest
-import src.reports as reports
 
-from datetime import datetime
-from src.reports import spending_by_category, save_reports
-from tests.conftest import reports_df
+import src.reports as reports
+from src.reports import save_reports, spending_by_category
 
 
 def test_spending_by_category_base(reports_df):
-    result_df = spending_by_category(reports_df, 'Еда','10.03.2024')
+    result_df = spending_by_category(reports_df, "Еда", "10.03.2024")
 
     result = result_df.to_dict()
-    assert result == {'Дата операции': {0: datetime(2024, 1, 1),
-                                        1: datetime(2024, 2, 15)},
-                      'Категория': {0: 'Еда',
-                                    1: 'Еда'},
-                      'Сумма': {0: 1500,
-                                1: 2000}}
+    assert result == {
+        "Дата операции": {0: datetime(2024, 1, 1), 1: datetime(2024, 2, 15)},
+        "Категория": {0: "Еда", 1: "Еда"},
+        "Сумма": {0: 1500, 1: 2000},
+    }
 
 
 def test_spending_by_category_no_date(reports_df):
-    result_df = spending_by_category(reports_df, 'Еда')
+    result_df = spending_by_category(reports_df, "Еда")
 
     result = result_df.to_dict()
-    assert result == {'Дата операции': {3: datetime(2026,2,1)},
-                      'Категория': {3: 'Еда'},
-                      'Сумма': {3: 500}}
+    assert result == {"Дата операции": {3: datetime(2026, 2, 1)}, "Категория": {3: "Еда"}, "Сумма": {3: 500}}
 
 
 def test_spending_by_category_non_cat(reports_df):
-    result_df = spending_by_category(reports_df, '','10.03.2024')
+    result_df = spending_by_category(reports_df, "", "10.03.2024")
 
     result = result_df.to_dict()
-    assert result == {'Дата операции': {}, 'Категория': {}, 'Сумма': {}}
+    assert result == {"Дата операции": {}, "Категория": {}, "Сумма": {}}
 
 
 def test_spending_by_category_wrong_df():
-    reports_df = pd.DataFrame([{
-        'Операция' : datetime(2026, 2,5),
-        'Оплата' : 2222,
-        'Кэшбек' : 0.2
-    }])
+    reports_df = pd.DataFrame([{"Операция": datetime(2026, 2, 5), "Оплата": 2222, "Кэшбек": 0.2}])
     with pytest.raises(ValueError):
-        spending_by_category(reports_df, '')
+        spending_by_category(reports_df, "")
 
 
 def test_save_reports_base(tmp_path):
-    @save_reports('test_reports')
+    @save_reports("test_reports")
     def example_report():
         return pd.DataFrame({"a": [1, 2]})
 
@@ -63,7 +56,7 @@ def test_save_reports_base(tmp_path):
         data = json.load(f)
 
     assert "a" in data
-    assert data["a"] == {'0': 1, '1': 2}
+    assert data["a"] == {"0": 1, "1": 2}
 
 
 def test_save_reports_error(tmp_path):
@@ -92,8 +85,7 @@ def test_save_reports_erro():
 
     example_report()
 
-    with open("/Users/vadimsemenov/PycharmProjects/App_for_Bank/data/reports.json",
-              "r", encoding="utf-8") as f:
+    with open("/Users/vadimsemenov/PycharmProjects/App_for_Bank/data/reports.json", "r", encoding="utf-8") as f:
         data = json.load(f)
 
     assert data["a"] == [1, 2]
