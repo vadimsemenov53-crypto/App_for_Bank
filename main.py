e45829e3af43d03f46891a8cfce9c67dc7c35a7b
Vadim Semenov <vadimsemenov53@gmail.com>
2026-02-21 14:15:05 +0300

    clean code main.py

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
-from src.views import build_response_json
-from src.utils import get_data_transactions_from_xlsx
-from src.reports import spending_by_category
 from src.logger_config import get_file_logger
+from src.reports import spending_by_category
+from src.utils import get_data_transactions_from_xlsx
+from src.views import build_response_json
 
 logger = get_file_logger(__name__, "main.log")
 
+
 def main() -> None:
     logger.info("Запущено основное приложение.")
-    print(
-        """
+    print("""
         Программа: Привет! Добро пожаловать в Приложение для анализа банковских операций.
         Данное приложение анализирует транзакции из XLSX-файла
-        """
-    )
+        """)
 
     try:
         while True:
             print("""
-            Программа: 
+            Программа:
             Выберите действие:
             1 — Анализ операций
             2 — Отчет по категории за 3 месяца
@@ -43,7 +42,7 @@ def main() -> None:
                     Передайте дату в формате: 20.05.2020,
                     Вы можете оставить окно пустым, в таком случае будет произведен анализ
                     от текущей даты.""")
-                date_user = str(input('Пользователь: '))
+                date_user = str(input("Пользователь: "))
 
                 print("Программа: Напишите категорию для поиска.")
                 category_user = str(input("Пользователь: ")).strip()
@@ -53,11 +52,11 @@ def main() -> None:
                 result_df = get_data_transactions_from_xlsx()
 
                 print("Программа: Сохранить отчет? (да/нет).")
-                save_answer = str(input('Пользователь: ')).strip().lower()
+                save_answer = str(input("Пользователь: ")).strip().lower()
 
                 file_name_user = None
 
-                if save_answer == 'да':
+                if save_answer == "да":
                     print("Программа: Введите имя файла (или Enter для стандартного).")
                     file_name_user = str(input("Пользователь: ")).strip()
 
@@ -66,14 +65,9 @@ def main() -> None:
 
                 logger.info("Запись отчета в файл: %s.json", file_name_user)
 
-
                 report = spending_by_category(
-                    result_df,
-                    category_user,
-                    date_user,
-                    save=(save_answer.lower() == 'да'),
-                    file_name=file_name_user
-                    )
+                    result_df, category_user, date_user, save=(save_answer.lower() == "да"), file_name=file_name_user
+                )
 
                 logger.info("Отчет сформирован -> вывод в консоль")
                 print(report)
